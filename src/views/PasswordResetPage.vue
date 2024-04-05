84d8d92bbfbbb3e391d445d167392ff37c449d75
Kris <kcwright@gmail.com>
2024-04-05 12:41:37 -0400

    new notification and bugfixes on pw reset pages

--- src/views/PasswordResetPage.vue
+++ src/views/PasswordResetPage.vue
@@ -1,101 +1,111 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="12" sm="8" md="6">
-          <v-card>
-            <v-card-title>Password Reset</v-card-title>
-            <v-card-text>
-              <v-form @submit.prevent="resetPassword">
-                <v-text-field v-model="resetData.newPassword" label="New Password" type="password"></v-text-field>
-                <v-text-field v-model="resetData.confirmPassword" label="Confirm New Password" type="password"></v-text-field>
-                <v-btn type="submit" color="primary">Reset Password</v-btn>
-              </v-form>
-              <v-alert v-if="notification" :type="notification.type">{{ notification.message }}</v-alert>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  import { ref, onMounted } from 'vue';
-  import { useAccounts } from '@/composables/useAccounts.js';
-  import { useRoute } from 'vue-router'; // Import useRoute from vue-router
-  
-  export default {
-    setup() {
-      const { attemptPasswordReset } = useAccounts();
-      const notification = ref(null);
-      const resetData = ref({
-        resetToken: null, // Initialize resetToken
-        newPassword: null,
-        confirmPassword: null,
-      });
-      const route = useRoute(); // Access the current route
-  
-      // Extract reset token from the URL when the component is mounted
-      onMounted(() => {
-        const token = route.query.token; // Get the reset token from the query parameters
-        if (token) {
-          resetData.value.resetToken = token; // Assign reset token to resetData object
-        } else {
-          // Handle case when reset token is not provided in the URL
-          console.error('Reset token not found in the URL');
-          // You may want to redirect the user to an error page or handle it differently
-        }
-      });
-  
-      const resetPassword = () => {
-        // Check if newPassword and confirmPassword are not empty
-        if (resetData.value.newPassword && resetData.value.confirmPassword) {
-            // Check if newPassword matches confirmPassword
-            if (resetData.value.newPassword === resetData.value.confirmPassword) {
-            attemptPasswordReset(resetData.value.resetToken, resetData.value.newPassword, resetData.value.confirmPassword)
-                .then(response => {
-                // Handle success or failure response
-                if (response.success) {
-                    notification.value = {
-                    type: 'success',
-                    message: 'Password reset successfully.'
-                    };
-                } else {
-                    notification.value = {
-                    type: 'error',
-                    message: 'Failed to reset password. Please try again.'
-                    };
-                }
-                })
-                .catch(error => {
-                console.error('Error resetting password:', error);
-                notification.value = {
-                    type: 'error',
-                    message: 'An error occurred while resetting your password. Please try again later.'
-                };
-                });
-            } else {
-            // If passwords do not match, show an error message
-            notification.value = {
-                type: 'error',
-                message: 'Passwords do not match. Please make sure both passwords are identical.'
-            };
-            }
-        } else {
-            // If newPassword or confirmPassword is empty, show an error message
-            notification.value = {
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title>Password Reset</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="resetPassword">
+              <v-text-field v-model="resetData.newPassword" label="New Password" type="password"></v-text-field>
+              <v-text-field v-model="resetData.confirmPassword" label="Confirm New Password" type="password"></v-text-field>
+              <v-btn type="submit" color="primary">Reset Password</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-snackbar
+      v-model="notification.show"
+      :timeout="5000"
+      :color="notification.type === 'success' ? 'success' : 'error'"
+    >
+      {{ notification.message }}
+    </v-snackbar>    
+  </v-container>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import { useAccounts } from '@/composables/useAccounts.js';
+import { useRoute } from 'vue-router'; // Import useRoute from vue-router
+
+export default {
+  setup() {
+    const { attemptPasswordReset } = useAccounts();
+    const notification = ref({ show: false, type: '', message: '' });
+    const resetData = ref({
+      resetToken: null, // Initialize resetToken
+      newPassword: null,
+      confirmPassword: null,
+    });
+    const route = useRoute(); // Access the current route
+
+    // Extract reset token from the URL when the component is mounted
+    onMounted(() => {
+      const token = route.query.token; // Get the reset token from the query parameters
+      if (token) {
+        resetData.value.resetToken = token; // Assign reset token to resetData object
+      } else {
+        // Handle case when reset token is not provided in the URL
+        console.error('Reset token not found in the URL');
+        // You may want to redirect the user to an error page or handle it differently
+      }
+    });
+
+    const resetPassword = () => {
+      // Check if newPassword and confirmPassword are not empty
+      if (resetData.value.newPassword && resetData.value.confirmPassword) {
+          // Check if newPassword matches confirmPassword
+          if (resetData.value.newPassword === resetData.value.confirmPassword) {
+          attemptPasswordReset(resetData.value.resetToken, resetData.value.newPassword, resetData.value.confirmPassword)
+              .then(response => {
+              // Handle success or failure response
+              if (response.success) {
+                  notification.value = {
+                  show: true,                    
+                  type: 'success',
+                  message: 'Password reset successfully.'
+                  };
+              } else {
+                  notification.value = {
+                  show: true, 
+                  type: 'error',
+                  message: 'Failed to reset password. Please try again.'
+                  };
+              }
+              })
+              .catch(error => {
+              console.error('Error resetting password:', error);
+              notification.value = {
+                  show: true, 
+                  type: 'error',
+                  message: 'An error occurred while resetting your password. Please try again later.'
+              };
+              });
+          } else {
+          // If passwords do not match, show an error message
+          notification.value = {
+              show: true, 
+              type: 'error',
+              message: 'Passwords do not match. Please make sure both passwords are identical.'
+          };
+          }
+      } else {
+          // If newPassword or confirmPassword is empty, show an error message
+          notification.value = {
+            show: true, 
             type: 'error',
             message: 'Please enter both new password and confirm password.'
-            };
-        }
-        };
-
-  
-      return {
-        notification,
-        resetData,
-        resetPassword
+          };
+      }
       };
-    }
-  };
-  </script>
-  
+
+
+    return {
+      notification,
+      resetData,
+      resetPassword
+    };
+  }
+};
+</script>
--- src/views/PasswordResetRequestPage.vue
+++ src/views/PasswordResetRequestPage.vue
@@ -1,68 +1,80 @@
 <template>
-    <v-container>
-      <v-row justify="center">
-        <v-col cols="12" sm="8" md="6">
-          <v-card>
-            <v-card-title>Password Reset Request</v-card-title>
-            <v-card-text>
-              <p>Are you sure you want to request a password reset for your account?</p>
-              <v-btn @click="requestPasswordReset" color="primary">Request Password Reset</v-btn>
-              <v-alert v-if="notification" :type="notification.type">{{ notification.message }}</v-alert>
-            </v-card-text>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
-  
-  <script>
-  import { useAccounts } from '@/composables/useAccounts.js';
-  import { ref } from 'vue';
-  
-  export default {
-    setup() {
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="6">
+        <v-card>
+          <v-card-title>Password Reset Request</v-card-title>
+          <v-card-text>
+            <p>Are you sure you want to request a password reset for your account?</p>
+            <v-form @submit.prevent="requestPasswordReset">
+              <v-text-field v-model="usernameOrEmail" label="Username or Email" required :disabled="isLoggedIn" :rules="usernameOrEmailRules"></v-text-field>
+              <v-btn type="submit" color="primary">Request Password Reset</v-btn>
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-snackbar
+      v-model="notification.show"
+      :timeout="5000"
+      :color="notification.type === 'success' ? 'success' : 'error'"
+    >
+      {{ notification.message }}
+    </v-snackbar>
+  </v-container>
+</template>
 
-      const account = useAccounts();
-      const notification = ref(null);
-  
-      const requestPasswordReset = () => {
-        //console.log("Email: ",account.email.value);
-        if (account.email.value) {
-          account.sendPasswordResetRequest(account.email.value)
-            .then(response => {
-              // Handle success or failure response
-              if (response.success) {
-                notification.value = {
-                  type: 'success',
-                  message: 'Password reset request sent successfully.'
-                };
-              } else {
-                notification.value = {
-                  type: 'error',
-                  message: 'Failed to send password reset request. Please try again.'
-                };
-              }
-            })
-            .catch(error => {
-              console.error('Error requesting password reset:', error);
-              notification.value = {
-                type: 'error',
-                message: 'An error occurred while processing your request. Please try again later.'
-              };
-            });
+<script>
+import { ref, computed } from 'vue';
+import { useAccounts } from '@/composables/useAccounts.js';
+import { useRouter } from 'vue-router';
+
+export default {
+  setup() {
+    const { isLoggedIn, email, sendPasswordResetRequest } = useAccounts();
+    const router = useRouter();
+    const notification = ref({ show: false, type: '', message: '' });
+    const usernameOrEmail = ref(isLoggedIn.value ? email.value : '');
+    
+    // Rules for the username or email field
+    const usernameOrEmailRules = computed(() => {
+      return [
+        v => !!v || 'Username or Email is required',
+      ];
+    });
+
+    const requestPasswordReset = async () => {
+      try {
+        const response = await sendPasswordResetRequest(usernameOrEmail.value);
+        if (response.success) {
+          notification.value = {
+            show: true,
+            type: 'success',
+            message: response.message
+          };
         } else {
           notification.value = {
+            show: true,
             type: 'error',
-            message: 'Unable to retrieve email. Please try again.'
+            message: response.message
           };
         }
-      };
-  
-      return {
-        notification,
-        requestPasswordReset
-      };
-    }
-  };
-  </script>
-  
+      } catch (error) {
+        console.error('Error requesting password reset:', error);
+        notification.value = {
+          show: true,
+          type: 'error',
+          message: 'An error occurred while processing your request. Please try again later.'
+        };
+      }
+    };
+
+    return {
+      usernameOrEmail,
+      usernameOrEmailRules,
+      notification,
+      requestPasswordReset
+    };
+  }
+};
+</script>
